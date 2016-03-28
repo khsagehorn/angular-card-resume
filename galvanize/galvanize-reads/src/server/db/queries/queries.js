1faff5aa0d9e9db74a78c31248ef822948c8c5f9
khsagehorn <huntersagehorn@gmail.com>
2016-03-28 13:47:41 -0600

    added queries and began routing

---
+++ galvanize/galvanize-reads/src/server/db/queries/queries.js
@@ -0,0 +1,31 @@
+var knex = require('../db/knex');
+
+function Books(){
+  return knex('books');
+}
+
+function Authors(){
+  return knex('authors');
+}
+
+module.exports = {
+  getBooks: function(){
+    return Books().select();
+  },
+
+  getBook: function(){
+    return Books().select().where('id', id);
+  },
+
+  getAuthors: function(){
+    return Authors().select();
+  },
+
+  getAuthor: function(){
+    return Authors().select().where('id', id);
+  },
+
+  getAuthorsForBooks
+
+
+};
--- galvanize/galvanize-reads/src/server/routes/index.js
+++ galvanize/galvanize-reads/src/server/routes/index.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
+var queries = require('../db/queries');
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Galvanize Reads' });
 });
 
 router.get('/books', function(req, res, next) {
-  res.render('/books/books', { title: 'Express' });
+  queries.getBooks().then(function(books){
+    queries.getAuthors().select().where('')
+    res.render('/books/index', { title: 'Books', {books: data} });
+
+  })
+
+});
+
+router.get('/authors', function(req, res, next) {
+  res.render('/authors/index', { title: 'Authors' });
 });
 
 module.exports = router;
